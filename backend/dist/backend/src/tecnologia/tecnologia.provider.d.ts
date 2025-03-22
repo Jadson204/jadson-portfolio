c42d96fb09c6762f63e61ef3e88a69f6d956b0fe
Jadson204 <jadsonhenrique2004@gmail.com>
2025-03-21 22:46:42 -0300

    feat(backend): deploy no fly.io

--- backend/dist/backend/src/tecnologia/tecnologia.provider.d.ts
+++
@@ -1,8 +0,0 @@
-import { Tecnologia } from '@core';
-import { PrismaProvider } from 'src/db/prisma.provider';
-export declare class TecnologiaProvider {
-    private readonly prisma;
-    constructor(prisma: PrismaProvider);
-    obterTodas(): Promise<Tecnologia[]>;
-    obterDestaques(): Promise<Tecnologia[]>;
-}
