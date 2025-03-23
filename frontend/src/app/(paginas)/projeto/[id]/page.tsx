bd8d1e9e9a61db13f402499cff30475fb2f97c91
Jadson204 <jadsonhenrique2004@gmail.com>
2025-03-23 20:51:06 -0300

    feat(frontend): implementacao do frontend

---
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -0,0 +1,32 @@
+import Readme from "@/components/projetos/Readme";
+import Cabecalho from "@/components/shared/Cabecalho";
+import CarrosselImagens from "@/components/shared/CarrosselImagens";
+import Container from "@/components/shared/Container";
+import ConteudoMD from "@/components/shared/ConteudoMD";
+import Tecnologias from "@/components/tecnologias/Tecnologias";
+import { obterReadme } from "@/functions/github";
+import { obterProjeto } from "@/functions/projetos";
+
+export default async function PaginaProjeto(props: { params: Promise<{ id: string }> }) {
+    const { id } = await props.params
+    const projeto = await obterProjeto(id)
+
+    if (!projeto) return null
+
+    const readme = await obterReadme(projeto.repositorio)
+
+    return (
+        <div className="bg-black">
+            <Cabecalho />
+            <Container className="py-7 flex flex-col items-center gap-10">
+                <h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
+                <div className="flex flex-col items-center">
+
+                </div>
+                <CarrosselImagens imagens={projeto.imagens.slice(1)} />
+                <Tecnologias lista={projeto.tecnologias} tamamhoMenor />
+                <Readme markdown={readme} />
+            </Container>
+        </div>
+    )
+}
---
+++ frontend/src/components/shared/Cabecalho.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Container from "./Container";
+import Link from "next/link";
+import Menu from "./Menu";
+
+export default function Cabecalho() {
+    return (
+        <header className="w-full flex items-center h-16 bg-black/50">
+            <Container className="flex-1 flex justify-center sm:justify-between items-center">
+                <div className="flex items-center gap-10">
+                    <Link href="/" className="hideen sm:block">
+                        <Image src="/logo.svg" alt="Logo" width={80} height={0} />
+                    </Link>
+                    <Menu />
+                </div>
+                <div className="hidden sm:flex items-center">
+                    <Link href="https://www.linkedin.com/in/jadson-henrique-33a41b241"
+                        target="blank"
+                        className="bg-red-500 rounded-full px-7 py-1 text-sm font-bold">
+                        Perfil
+                    </Link>
+                </div>
+            </Container>
+        </header>
+    )
+}
