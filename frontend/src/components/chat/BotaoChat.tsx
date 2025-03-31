8e19bf4830a6f1173843b1d65d7c3bcc203a93fe
Jadson204 <jadsonhenrique2004@gmail.com>
2025-03-31 17:07:26 -0300

    bugfix(frontend); ajustando o campo de texto na janela de IA

--- frontend/src/components/chat/BotaoChat.tsx
+++ frontend/src/components/chat/BotaoChat.tsx
@@ -1,24 +1,26 @@
-import Image from "next/image";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import JanelaChat from "./JanelaChat";
+import Image from "next/image"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import JanelaChat from "./JanelaChat"
 
 export default function BotaoChat() {
-    return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <div className="fixed bottom-5 right-5 cursor-pointer">
-                    <Image src="/chat.svg" alt="Chat" width={50} height={50} />
-                </div>
-            </PopoverTrigger>
-            <PopoverContent
-                side="top"
-                align="end"
-                sideOffset={10}
-                className="w-[400px] sm:w-[500px] max-h-[85vh] overflow-hidden flex flex-col"
-                style={{ backgroundColor: "transparent", border: "none" }}
-            >
-                <JanelaChat />
-            </PopoverContent>
-        </Popover>
-    )
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<div className="fixed bottom-5 right-5 cursor-pointer">
+					<Image src="/chat.svg" alt="Chat" width={50} height={50} />
+				</div>
+			</PopoverTrigger>
+			<PopoverContent
+				side="top"
+				align="end"
+				sideOffset={10}
+				className="w-[400px] sm:w-[500px] h-[80vh] flex flex-col overflow-hidden"
+				style={{ backgroundColor: "transparent", border: "none" }}
+			>
+				<div className="h-full flex flex-col">
+					<JanelaChat />
+				</div>
+			</PopoverContent>
+		</Popover>
+	)
 }
